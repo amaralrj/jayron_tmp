bf0bbb68bdd5b7dd0f3cf1bda46fc59f7beb5a29
Pedro Licio <pedro.licio@acotel.com>
2014-02-17 17:38:09 -0300

    Email opcional.

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -1,7 +1,8 @@
+# coding: utf-8
 from django.contrib import admin
 from eventex.subscriptions.models import Subscription
 from django.utils.datetime_safe import datetime
-from django.utils.translation import ugettext as _
+from django.utils.translation import ungettext, ugettext as _
 
 
 class SubscriptionAdmin(admin.ModelAdmin):
@@ -15,6 +16,18 @@ class SubscriptionAdmin(admin.ModelAdmin):
 
     subscribed_today.short_description = _(u'Inscrito hoje?')
     subscribed_today.boolean = True
+    actions = ['mark_as_paid']
+
+    def mark_as_paid(self, request, queryset):
+        count = queryset.update(paid=True)
+        msg = ungettext(
+                u'%d inscrição foi marcada como paga.',
+                u'%d inscrições foram marcadas como pagas.',
+                count
+                )
+        self.message_user(request, msg % count)
+
+    mark_as_paid.short_description = _('Marcar como pago')
 
 
 admin.site.register(Subscription, SubscriptionAdmin)
